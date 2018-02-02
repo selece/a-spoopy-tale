e9ad26663e6849b3c2be482fa16d50c0731d5294
selece <jonathan.liaw@gmail.com>
2018-02-01 19:48:44 -0700

    Built basic nav remembering for player object. YAY CONTEXTS!
    Signed-off-by: selece <jonathan.liaw@gmail.com>

--- src/js/spoopy.game.js
+++ src/js/spoopy.game.js
@@ -5,27 +5,40 @@ require.config({
     paths: {
         'jquery': '../jquery.min',
         'underscore': '../underscore.min',
+        'pubsub': '../pubsub',
     }
 });
 
 define(
-    ['jquery', 'underscore', 'spoopy.rooms', 'spoopy.items', 'spoopy.player', 'spoopy.engine.button'],
-    ($, _, rooms, items, player, engine_button) => {
-        let current_room = 'Kitchen';
-        
+    ['jquery', 'underscore', 'pubsub', 'spoopy.rooms', 'spoopy.items', 'spoopy.player',],
+    ($, _, PS, rooms, items, Player) => {
+    
         $(document).ready(() => {
-            $('#header h1').text(current_room);
+            let player = new Player();
+            player.pickup_item('Socks');
+            player.drop_item('Socks');
+
+            $('#header h1').text(player.loc);
             // $('#content p').text(rooms.get_description(current_room, {has_item: ['Skull']}));
-            $('#content p').text(rooms.get_description(current_room));
+            $('#content p').text(rooms.get_description(player.loc));
 
             _.mapObject(
                 rooms.rooms,
-                room => $('#nav ul').append(
-                    `<li><a href="#">${room.name}</a></li>`
-                )
+                room => $('#nav ul').append(build_room_nav_button(room))
             );
         });
 
-        player.pickup_item('Skull');
-        player.drop_item('Skull');
+        let engine_nav_handler = (channel, msg) => {
+            console.log(channel, msg);
+        };
+
+        let engine_nav_handler_sub = PS.subscribe('ENGINE_NAV_CLICK', engine_nav_handler);
+
+        let build_room_nav_button = (room) => {
+            return $('<li>').html($('<a>', {
+                text: room.name,
+                href: '#',
+                click: () => PS.publish('ENGINE_NAV_CLICK', room.name)
+            }));
+        };
 });
