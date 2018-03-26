060823a2b286deedd3540087c220eb7e35ee88ba
selece <jonathan.liaw@gmail.com>
2018-03-25 20:43:32 -0600

    Fixing webpack config for TravisCI - slash directions

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,51 @@
-const html_webpack_plugin = require('html-webpack-plugin');
-const clean_webpack_plugin = require('clean-webpack-plugin');
+const html_webpack_plugin = require("html-webpack-plugin");
+const clean_webpack_plugin = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        bundle: __dirname + '\\src\\index.jsx'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/, /\.spec.(js|jsx)$/],
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: 'sass-loader'
-            }
-        ],
-        rules: [
-            {
-                test: /\.(s*)css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/, /\.spec.(js|jsx)$/],
-                use: 'babel-loader'
-            }
-        ]
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '\\build',
-    },
-    plugins: [
-        new clean_webpack_plugin(
-            ['build/*.*'],
-            {
-                verbose: true,
-                watch: true,
-                beforeEmit: true
-            }
-        ),
-        new html_webpack_plugin({
-            template: __dirname + '\\src\\index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
+  entry: {
+    bundle: __dirname + "/src/index.jsx"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /\.spec.(js|jsx)$/],
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: "sass-loader"
+      }
     ],
-    cache: true,
-    devtool: 'source-map',
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, /\.spec.(js|jsx)$/],
+        use: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/build"
+  },
+  plugins: [
+    new clean_webpack_plugin(["build/*.*"], {
+      verbose: true,
+      watch: true,
+      beforeEmit: true
+    }),
+    new html_webpack_plugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ],
+  cache: true,
+  devtool: "source-map"
 };
